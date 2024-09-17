176abc08c432b8ca0bd9b9b75d9aad260c34fb69
Macs-Scripts <167629435+MacSyst@users.noreply.github.com>
2024-09-17 20:04:27 +0200

    Update config.lua

--- Vision_Lock/config.lua
+++ Vision_Lock/config.lua
@@ -9,7 +9,7 @@ Vision.Debug                    = true  -- print the filename
 Vision.Console                  = true  -- print to console
 Vision.UpdateCheck              = true  -- check for updates
 Vision.CallbackTest             = false -- prints the Callback for testing
-Vision.StartUp                  = false -- infos about the script and the author
+Vision.StartUp                  = true -- infos about the script and the author
 
 -- -----------------------------------
 -- Key Mapping
@@ -22,4 +22,4 @@ Vision.AdminKey                 = "O"
 -- Supports
 -- -----------------------------------
 
-Vision.okoknotify               = true --https://okok.tebex.io/package/4724993
+Vision.okoknotify               = false --https://okok.tebex.io/package/4724993
