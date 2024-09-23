b22cc2265530ded5f0f05f3ba1394a76c71f92dd
Vision-Service <167629435+MacSyst@users.noreply.github.com>
2024-09-23 16:04:52 +0200

    Add files via upload

---
+++ Vision_Keylock/config.lua
@@ -0,0 +1,34 @@
+Vision = {}
+
+
+-- -----------------------------------
+-- Vision Settings
+-- -----------------------------------
+
+Vision.Debug                    = true  -- print the filename
+Vision.Console                  = true  -- print to console
+Vision.UpdateCheck              = true  -- check for updates
+Vision.CallbackTest             = false -- prints the Callback for testing
+Vision.StartUp                  = true -- infos about the script and the author
+
+-- -----------------------------------
+-- Key Mapping
+-- -----------------------------------
+
+Vision.Key                      = "U"  --/key
+Vision.AdminKey                 = "O"  --/akey
+Vision.GangKey                  = "I"  --/gkey
+
+-- -----------------------------------
+-- Supports
+-- -----------------------------------
+
+Vision.okoknotify               = false --https://okok.tebex.io/package/4724993
+
+-- -----------------------------------
+-- Vehicles
+-- -----------------------------------
+
+Vision.Gang = {
+    ["ADMINCAR"] = "police"
+}
